b009ca9f7f90de316475655faeb5d2f73aa1d760
raunakrocks <raunaktalwar00@gmail.com>
2019-02-05 12:51:09 +0530

    add fabric-ui package and use document card fabric ui component inside app.js.

--- documentcardbasic-demo/src/App.js
+++ documentcardbasic-demo/src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  DocumentCard,
+  DocumentCardPreview,
+  DocumentCardTitle,
+  DocumentCardActivity,
+  IDocumentCardPreviewProps
+} from 'office-ui-fabric-react/lib/DocumentCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <DocumentCard onClickHref='http://google.com'>
+            <DocumentCardPreview
+              previewImages={ [
+                {
+                  previewImageSrc: require('./documentpreview.png'),
+                  iconSrc: require('./iconppt.png'),
+                  width: 318,
+                  height: 196,
+                  accentColor: '#ce4b1f'
+                }
+              ] }
+            />
+            <DocumentCardTitle title='This is document card title text'/>
+            {/* <DocumentCardActivity
+              activity='Created Feb 23, 2016'
+              people={
+                [
+                  { name: 'Raunak', profileImageSrc: require('./avatarkat.png') }
+                ]
+              }
+              /> */}
+          </DocumentCard>
       </div>
     );
   }
