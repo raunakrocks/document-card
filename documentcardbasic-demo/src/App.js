545b6c209106e9e5f73c629f5e0b14c4cf3c2a3a
raunakrocks <raunaktalwar00@gmail.com>
2019-02-05 14:19:05 +0530

    Move out basic document card code to a seprate file.

--- documentcardbasic-demo/src/App.js
+++ documentcardbasic-demo/src/App.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {
-  DocumentCard,
-  DocumentCardPreview,
-  DocumentCardTitle,
-  DocumentCardActivity,
-  IDocumentCardPreviewProps
-} from 'office-ui-fabric-react/lib/DocumentCard';
+import BasicDocumentCard from './BasicDocumentCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <DocumentCard onClickHref='http://google.com'>
-            <DocumentCardPreview
-              previewImages={ [
-                {
-                  previewImageSrc: require('./documentpreview.png'),
-                  iconSrc: require('./iconppt.png'),
-                  width: 318,
-                  height: 196,
-                  accentColor: '#ce4b1f'
-                }
-              ] }
-            />
-            <DocumentCardTitle title='This is document card title text'/>
-            {/* <DocumentCardActivity
-              activity='Created Feb 23, 2016'
-              people={
-                [
-                  { name: 'Raunak', profileImageSrc: require('./avatarkat.png') }
-                ]
-              }
-              /> */}
-          </DocumentCard>
+          <BasicDocumentCard/>
       </div>
     );
   }
---
+++ documentcardbasic-demo/src/BasicDocumentCard.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+    DocumentCard,
+    DocumentCardPreview,
+    DocumentCardTitle,
+    DocumentCardActivity,
+    IDocumentCardPreviewProps
+} from 'office-ui-fabric-react/lib/DocumentCard';
+
+class BasicDocumentCard extends Component {
+
+    render() {
+        return (
+            <div>
+                 <DocumentCard onClickHref='http://google.com'>
+                    <DocumentCardPreview
+                    previewImages={ [
+                        {
+                        previewImageSrc: require('./documentpreview.png'),
+                        iconSrc: require('./iconppt.png'),
+                        width: 318,
+                        height: 196,
+                        accentColor: '#ce4b1f'
+                        }
+                    ] }
+                    />
+                    <DocumentCardTitle title='This is document card title text'/>
+                    <DocumentCardActivity
+                    activity='Created Feb 23, 2016'
+                    people={
+                        [
+                        { name: 'Raunak', profileImageSrc: require('./avatarkat.png') }
+                        ]
+                    }
+                    />
+                </DocumentCard>
+        </div>
+        );
+    }
+}
+export default BasicDocumentCard;
